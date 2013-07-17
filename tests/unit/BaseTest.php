b4e14f2218e71703dbf7e15f2d7a1d4efe904461
Jáchym Toušek <enumag@gmail.com>
2013-07-17 10:23:06 +0200

    Improved BaseTest

--- tests/unit/BaseTest.php
+++ tests/unit/BaseTest.php
@@ -5,13 +5,10 @@ namespace Tests\Arachne\Verifier;
 abstract class BaseTest extends \Codeception\TestCase\Test
 {
 
-	protected function _before()
-	{
-	}
-
-	protected function _after()
+	protected function tearDown()
 	{
 		\Mockery::close();
+		parent::tearDown();
 	}
 
 }
