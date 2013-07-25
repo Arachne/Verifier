d3b17e1c1060a60c24661c2beee6d4d3c8e27086
Jáchym Toušek <enumag@gmail.com>
2013-07-25 10:31:59 +0200

    Disabled codeception silent mode

--- tests/runtests.bat
+++ tests/runtests.bat
@@ -3,4 +3,4 @@
 call "vendor/bin/phpcs.bat" -p --standard=vendor/arachne/coding-style/ruleset.xml src
 call "vendor/bin/phpcs.bat" -p --standard=vendor/arachne/coding-style/ruleset.xml tests
 call "vendor/bin/codecept.bat" build
-call "vendor/bin/codecept.bat" run --silent
+call "vendor/bin/codecept.bat" run
