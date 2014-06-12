e02a10da855b67a1222cd028f08836deeabc1163
Jáchym Toušek <enumag@gmail.com>
2014-06-12 22:15:25 +0200

    Fixed tests shell runner

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,5 @@
 ./vendor/bin/phpcs -p --standard=vendor/arachne/coding-style/ruleset.xml src
 ./vendor/bin/phpcs -p --standard=vendor/arachne/coding-style/ruleset.xml --ignore=_temp tests
 ./vendor/bin/codecept build
-./vendor/bin/codecept run Unit
-./vendor/bin/codecept run Integration
+./vendor/bin/codecept run unit
+./vendor/bin/codecept run integration
